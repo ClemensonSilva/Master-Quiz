830bfd23cb2a4fc917345fbb350180a4c5daf759
ClemensonSilva <clemesonsilva736@gmail.com>
2025-10-07 14:01:28 -0300

    Definicao das estruturas do projeto e do docker

---
+++ projeto-quiz/src/main/java/br/com/edu/ufersa/projeto_quiz/ProjetoQuizApplication.java
@@ -0,0 +1,13 @@
+package br.com.edu.ufersa.projeto_quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoQuizApplication.class, args);
+	}
+
+}
---
+++ projeto-quiz/src/test/java/br/com/edu/ufersa/projeto_quiz/ProjetoQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edu.ufersa.projeto_quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
